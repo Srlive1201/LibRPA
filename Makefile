2aa89bf598ea703d076ee83c72cd756133d47a34
minyez <minyez@iphy.ac.cn>
2022-04-18 22:54:38 +0800

    calculate and dump sine transform matrix
    The grid script is re-organized to isolate variables.
    
    Both cosine and sine transformation matrix are briefly checked after
    modification, but may need more check on the final correlation
    energy.
    
    Also add -O2 as a default compilation flag

--- Makefile
+++ Makefile
@@ -6,14 +6,15 @@ EXENAME = chi0_main.exe
 
 LAPACK_DIR = $(MKLROOT)/lib/intel64
 LAPACK_LIB = -L$(LAPACK_DIR) -lmkl_intel_lp64 -lmkl_sequential -lmkl_core
-OPTS = -std=c++11 -qopenmp
+OPTS = -g -O2 -std=c++11 -qopenmp
+#OPTS = -g -O2 -std=c++11 -qopenmp -ffast-math -march=native
 #OPTS_MPI= -cxx=$(CPP)
 
 global = cal_periodic_chi0.o aperiodic_chi0.o Gauss_Quadrature.o vector3_order.o matrix.o complexmatrix.o matrix3.o input.o parallel_mpi.o global_class.o
 object = lib_main.o $(global)
 
 $(EXENAME): $(object)
-	$(CPP_MPI) -g -o $@ $^ $(LAPACK_LIB) $(OPTS)
+	$(CPP_MPI) -o $@ $(OPTS) $^ $(LAPACK_LIB)
 .cpp.o:
 	$(CPP_MPI) $(OPTS) -c $< -o $@
 
--- minimax_grid/GreenX/generate_local_grid.py
+++ minimax_grid/GreenX/generate_local_grid.py
@@ -1,86 +1,137 @@
-import os
-from scipy.optimize import minimize,Bounds,least_squares
-import numpy as np
-import matplotlib.pyplot as plt
+"""write the minimax grids and cosine/sine transformation matrix"""
+from __future__ import print_function
 import math
 import sys
+import os
+import numpy as np
+from scipy.optimize import least_squares
+# from scipy.optimize import minimize,Bounds,least_squares
+# import matplotlib.pyplot as plt
 
-def read_grid(N,grid_type):
-    if grid_type == 'f':
-        fm=str(N)+"_freq_points.dat"
-    elif grid_type == 't':
-        fm=str(N)+"_time_points.dat"
-    with open(path+fm,'r') as ff:
-        while True:
-            lines=ff.readline()
-            if lines[:6]=='Erange':
-                ss,lb,ub=lines.split()
-                if R>=eval(lb) and R<=eval(ub):
-                    lines=next(ff)
-                    i=2*N
-                    with open('local_'+fm,'w') as fw:
-                        while i>0:
+fnerror = IOError
+if sys.version_info.major >= 3:
+    fnerror = FileNotFoundError
+
+
+def _fun(x, y):
+    """1/(x+iy) + 1/(x-iy)"""
+    return (2*x)/(x**2+y**2)
+
+
+def read_grid(ngrids, erange):
+    """read the tabulated freq and time grids and write to local files
+
+    Args:
+        ngrids (float): number of grids
+        erange (float): energy range
+
+    Returns:
+        freq and time grids.
+        (2*ngrids,) array, first ngrids as points and left are weights.
+    """
+    path = os.path.dirname(__file__) + "/"
+
+    grids = np.zeros((2, 2*ngrids))
+
+    fn_freq = str(ngrids)+"_freq_points.dat"
+    fn_time = str(ngrids)+"_time_points.dat"
+    for ifn, fn in enumerate([fn_freq, fn_time]):
+        try:
+            with open(path+fn,'r') as ff:
+                while True:
+                    lines=ff.readline()
+                    if lines[:6]=='Erange':
+                        _, lb, ub = lines.split()
+                        if eval(lb) <= erange <= eval(ub):
                             lines=next(ff)
-                            print(lines.strip(),file=fw)
-                            if grid_type == 'f':
-                                omega.append(eval(lines.strip()))
-                            elif grid_type == 't':
-                                tau.append(eval(lines.strip()))
-                            i-=1
-                    break
+                            i=0
+                            while i<2*ngrids:
+                                lines=next(ff).strip()
+                                grids[ifn, i] = eval(lines)
+                                i+=1
+                            break
+        except fnerror:
+            raise fnerror("%d-point freq/time grids is not found" % ngrids)
+    for ifn, fn in enumerate([fn_freq, fn_time]):
+        with open('local_'+fn, 'w') as fw:
+            print("\n".join(str(x) for x in grids[ifn, :].tolist()),file=fw)
 
-def cosine_time2freq_grid(R):
+    return grids[0, :], grids[1, :]
+
+
+def time2freq_grid(omegas, taus, erange):
+    """generate and write the cosin/sine transformation matrix
+
+    Args:
+        omegas, taus: array generated from ``read_grid``
+        erange (float): energy range
+    """
     def tau_fun(p,x,tau):
-        sum=0
-        i=0
+        s = 0
+        i = 0
         while i< len(p):
-            sum+=p[i]*np.exp(-1*x*abs(tau[i]))
-            i=i+1
-        return sum
+            s += p[i]*np.exp(-x*abs(tau[i]))
+            i += 1
+        return s
 
-    def omega_fun(x,freq):
-        return (2*x)/(x**2+freq**2)
-    def error (p,x,y,tau):                    # 拟合残差
-        return tau_fun(p,x,tau)-y
-    num_points_per_magnitude=200
-    num_x_node=(int(math.log10(R))+1)*num_points_per_magnitude
-    num_x_node=max(num_x_node,len(tau)/2)
+    def error(p, x, y, tau):                    # 拟合残差
+        return tau_fun(p, x, tau) - y
 
-    multipicator=(R)**(1.0/(num_x_node-1.0))
-    xvalue=[multipicator**(i) for i in range(0,num_x_node)]
-    x=np.array(xvalue)
+    ngrids = len(taus) // 2
+    num_points_per_magnitude = 200
+    num_x_node = (int(math.log10(erange))+1)*num_points_per_magnitude
+    num_x_node = max(num_x_node, ngrids)
 
-    p0=[]
-    i=0
-    while i<len(tau)/2:
-        #p0.append(np.exp(0.1*i)-1)
-        p0.append(1)
-        i+=1
-    print(p0)
-    with open("INFO.txt","at") as f1:
-        print("Time_to_freq_transform   N:  ",N,"   Range_x: ",np.min(x),np.max(x),file=f1)
-    with open(str(N)+"_time2freq_grid.txt","wt") as f0:
-        omega_g=omega[:N]
+    x = np.power((erange)**(1.0/(num_x_node-1.0)),
+                 np.arange(0, num_x_node))
+
+    p0 = np.ones(ngrids)
+    with open("INFO.txt", "at") as f1:
+        print("Time_to_freq_transform   N = ", ngrids,
+              "   Range_x: ", np.min(x), np.max(x),
+              "n(x) = ", num_x_node, file=f1)
+        print("Cosine transform:", file=f1)
+    with open(str(ngrids)+"_time2freq_grid_cos.txt", "wt") as f0:
+        omega_g=omegas[:ngrids]
         for i_freq in omega_g:
-            print("Freq: ",i_freq,file=f0)
-            y=omega_fun(x,i_freq)
-            para_i=least_squares(error, p0,args=(x,y,tau))
+            print("Freq: ", i_freq, file=f0)
+            y=_fun(x, i_freq)
+            para_i=least_squares(error, p0, args=(x, y, taus))
             for gamma in para_i.x:
-                print(gamma,file=f0)
-            with open("INFO.txt","at") as f1:
-                print("Freq_point: ",i_freq,"         transform_err: ",np.max(abs(para_i.fun)),file=f1)
-    with open("INFO.txt","at") as f1:
-        print("Finish!",file=f1)
+                print(gamma, file=f0)
+            with open("INFO.txt", "at") as f1:
+                print("Freq_point: ", i_freq, "         transform_err: ",
+                      np.max(abs(para_i.fun)), file=f1)
+
+    with open("INFO.txt", "at") as f1:
+        print("", file=f1)
+        print("Sine transform:", file=f1)
+    with open(str(ngrids)+"_time2freq_grid_sin.txt", "wt") as f0:
+        omega_g=omegas[:ngrids]
+        for i_freq in omega_g:
+            print("Freq: ", i_freq, file=f0)
+            y=_fun(i_freq, x)
+            para_i=least_squares(error, p0, args=(x, y, taus))
+            for lambd in para_i.x:
+                print(lambd, file=f0)
+            with open("INFO.txt", "at") as f1:
+                print("Freq_point: ", i_freq, "         transform_err: ",
+                      np.max(abs(para_i.fun)), file=f1)
+
+    with open("INFO.txt", "at") as f1:
+        print("Finish!", file=f1)
+
+
+def main():
+    """main stream"""
+    ngrids=int(sys.argv[1])
+    erange=float(sys.argv[2])
+    print("N: ", ngrids)
+    print("R: ", erange)
+    omegas, taus = read_grid(ngrids, erange)
+    time2freq_grid(omegas, taus, erange)
+
 
 if __name__=='__main__':
-    N=int(sys.argv[1])
-    R=float(sys.argv[2])
-    print("N: ",N)
-    print("R: ",R)
-    omega=[]
-    tau=[]
-    path=os.path.dirname(__file__) + "/"
-    freq_grid=[]
-    read_grid(N,'f')
-    read_grid(N,'t')
-    cosine_time2freq_grid(R)
+    main()
+
