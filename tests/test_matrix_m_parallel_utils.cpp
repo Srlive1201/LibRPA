e976b5c7c4b1112fb07f9f9358f5c7389900e835
minyez <minyez.physchem@gmail.com>
2022-11-27 01:34:51 +0800

    test: power_hemat_blacs, now works for sp/dp

--- tests/test_matrix_m_parallel_utils.cpp
+++ tests/test_matrix_m_parallel_utils.cpp
@@ -78,12 +78,70 @@ void test_pgemm(const T &m1_lb, const T &m1_ub)
 }
 
 template <typename T>
-void test_power_hemat_blacs(const T &m1_lb, const T &m1_ub)
+void test_power_hemat_blacs(const T &m_lb, const T &m_ub)
 {
+    typedef T type;
+    typedef typename to_real<type>::type real_type;
+    const T thres =
+        std::is_same<typename to_real<type>::type, float>::value ? 1e-5 : 1e-14;
     blacs_ctxt_world_h.set_square_grid();
     assert(blacs_ctxt_world_h.nprocs == 4);
     assert(blacs_ctxt_world_h.nprows == 2);
     assert(blacs_ctxt_world_h.npcols == 2);
+
+    const int n = 6, nb = n / 2;
+    matrix_m<T> mat(0, 0, MAJOR::COL), mat_gather(0, 0, MAJOR::COL);
+    int pid_src, irsrc, icsrc;
+    pid_src = 2;
+    blacs_ctxt_world_h.get_pcoord(pid_src, irsrc, icsrc);
+    if (blacs_ctxt_world_h.myid == pid_src)
+    {
+        vector<real_type> evs {0.2, 0.5, 1.0, 2.0, 4.0};
+        mat = random_he_selected_ev(n, evs, MAJOR::COL);
+        mat_gather = mat;
+    }
+    auto pair_desc_m = prepare_array_desc_mr2d_src_and_all(blacs_ctxt_world_h, n, n, nb, nb, irsrc, icsrc);
+    auto mat_loc = init_local_mat<T>(pair_desc_m.second, MAJOR::COL);
+    auto eig_loc = init_local_mat<T>(pair_desc_m.second, MAJOR::COL);
+    // printf("eig_loc\n%s", str(eig_loc).c_str());
+    ScalapackConnector::pgemr2d_f(n, n,
+                                  mat.c, 1, 1, pair_desc_m.first.desc,
+                                  mat_loc.c, 1, 1, pair_desc_m.second.desc,
+                                  blacs_ctxt_world_h.ictxt);
+    // printf("mat_loc of PID %d before\n%s", blacs_ctxt_world_h.myid, str(mat_loc).c_str());
+    auto mat_loc_back = mat_loc;
+
+    real_type *W = new real_type [n];
+
+    power_hemat_blacs(mat_loc, pair_desc_m.second,
+                      eig_loc, pair_desc_m.second, W, real_type(1.0/3.0));
+    if (blacs_ctxt_world_h.myid == pid_src)
+    {
+        printf("Eigenvalues: ");
+        for (int i = 0; i < n; i++)
+            printf("%f ", W[i]);
+        printf("\n");
+    }
+    blacs_ctxt_world_h.barrier();
+    // printf("mat_loc of PID %d middle\n%s", blacs_ctxt_world_h.myid, str(mat_loc).c_str());
+    power_hemat_blacs(mat_loc, pair_desc_m.second,
+                      eig_loc, pair_desc_m.second, W, real_type(3.0));
+    // printf("eig_loc\n%s", str(eig_loc).c_str());
+    // printf("mat_loc of PID %d after\n%s", blacs_ctxt_world_h.myid, str(mat_loc).c_str());
+
+    ScalapackConnector::pgemr2d_f(n, n,
+                                  mat_loc.c, 1, 1, pair_desc_m.second.desc,
+                                  mat_gather.c, 1, 1, pair_desc_m.first.desc,
+                                  blacs_ctxt_world_h.ictxt);
+
+    if (blacs_ctxt_world_h.myid == pid_src)
+    {
+        printf("mat global at pid_src %d\n%s", pid_src, str(mat).c_str());
+        printf("mat gathered at pid_src %d\n%s", pid_src, str(mat_gather).c_str());
+        assert(fequal_array(n*n, mat.c, mat_gather.c, false, thres));
+    }
+
+    delete [] W;
 
     blacs_ctxt_world_h.exit();
 }
@@ -100,6 +158,9 @@ int main (int argc, char *argv[])
     test_pgemm<complex<double>>({-2, -1}, {1, 0});
     test_pgemm<float>(-2, 1);
     test_pgemm<complex<float>>({-2, -1}, {1, 0});
+
+    test_power_hemat_blacs<complex<double>>(0.0, {1.0, 1.0});
+    test_power_hemat_blacs<complex<float>>(0.0, {1.0, 2.0});
 
     MPI_Wrapper::finalize();
 
