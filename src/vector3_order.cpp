3ece9a90d9be501f97b4e029d40205113b0a831d
minyez <minyez.physchem@gmail.com>
2023-01-08 19:43:36 +0800

    feat: more Vector3_Order operators

--- src/vector3_order.cpp
+++ src/vector3_order.cpp
@@ -24,3 +24,19 @@ bool operator< ( const Vector3_Order<int> &v1, const Vector3_Order<int> &v2 )
 	else if ( v1.z > v2.z ) return false;
 	return false;
 }
+
+template<>
+bool operator== ( const Vector3_Order<double> &v1, const Vector3_Order<double> &v2 )
+{
+	constexpr double threshold = 1E-8;
+	if ( std::abs(v1.x - v2.x) > threshold ) return false;
+	if ( std::abs(v1.y - v2.y) > threshold ) return false;
+	if ( std::abs(v1.z - v2.z) > threshold ) return false;
+	return true;
+}
+
+template<>
+bool operator== ( const Vector3_Order<int> &v1, const Vector3_Order<int> &v2 )
+{
+    return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
+}
