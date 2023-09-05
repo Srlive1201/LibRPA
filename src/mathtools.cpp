db3a5d462c70090007fe407c08452fe1070cccd2
Minye Zhang <minyez.physchem@gmail.com>
2023-09-05 18:03:57 +0200

    move more Gauss_Quadrature functions into mathtool

--- src/mathtools.cpp
+++ src/mathtools.cpp
@@ -4,12 +4,15 @@
  */
 #include "mathtools.h"
 #include "constants.h"
+
 #include <cmath>
 #include <stdexcept>
 
 void GaussChebyshevI_unit(size_t N, double *nodes, double *weights)
 {
-    for ( int i = 0; i != (N+1)/2; i++)
+    const auto hNp1 = (N + 1) / 2;
+
+    for ( int i = 0; i != hNp1; i++)
     {
         nodes[i] = cos(PI*(i+0.5)/N);
         weights[i] = sqrt(1.0 - nodes[i] * nodes[i] ) * PI / N;
@@ -20,14 +23,15 @@ void GaussChebyshevI_unit(size_t N, double *nodes, double *weights)
 
 void GaussChebyshevII_unit(size_t N, double *nodes, double *weights)
 {
-    size_t Np1 = N + 1;
-    for ( int i = 0; i != Np1/2; i++)
+    const auto Np1 = N + 1;
+
+    for (int i = 0; i != Np1 / 2; i++)
     {
-        nodes[i] = cos(PI*(i+1)/Np1);
-        weights[i] = 1.0 / sqrt(1.0 - nodes[i] * nodes[i] ) * PI / Np1
-            * pow(sin(PI*(i+1)/Np1), 2);
-        nodes[N-1-i] = -nodes[i];
-        weights[N-1-i] = -weights[i];
+        nodes[i] = cos(PI * (i + 1) / Np1);
+        weights[i] = 1.0 / sqrt(1.0 - nodes[i] * nodes[i]) * PI / Np1 *
+                     pow(sin(PI * (i + 1) / Np1), 2);
+        nodes[N - 1 - i] = -nodes[i];
+        weights[N - 1 - i] = -weights[i];
     }
 }
 
@@ -35,13 +39,13 @@ void GaussLegendre_unit(size_t N, double *nodes, double *weights)
 {
     const double thres = 1e-16;
     const size_t maxiter = 1000;
+    const auto hNp1 = (N + 1) / 2;
+
     double x, y, dy, ratio;
-    size_t halfNp1;
-    halfNp1 = (N+1)/2;
-    for ( int i = 0; i != halfNp1; i++ )
+    for ( int i = 0; i != hNp1; i++ )
     {
         int ite = 0;
-        x = cos( PI *( i+1 -0.25 ) / ( N +0.5 ) );
+        x = cos(PI * (i + 1 - 0.25) / (N + 0.5));
         while ( ++ite != maxiter )
         {
             LegendreP_order_N(N, x, y, dy);
@@ -52,29 +56,8 @@ void GaussLegendre_unit(size_t N, double *nodes, double *weights)
         }
         if ( ite == maxiter) throw std::logic_error("Legendre Newton-Raphson not converged");
         // apply symmetry
-        nodes[N-1-i] = -nodes[i];
-        weights[N-1-i] = -weights[i];
-    }
-}
-
-void transform_GaussQuad_unit2x0inf(double x0, size_t N, double *nodes, double *weights)
-{
-    for ( int i = 0; i != N; i++ )
-    {
-        weights[i] = weights[i] /( ( 1.0- nodes[i] )*( 1.0- nodes[i] ) );
-        nodes[i] = 0.5* ( nodes[i] +1.0 ) / ( 1.0- nodes[i] ) + x0;
-    }
-}
-
-void transform_GaussQuad_unit2x0x1(double x0, double x1, size_t N, double *nodes, double *weights)
-{
-    double midp = (x0+x1)/2;
-    double hwid = (x1-x0)/2;
-
-    for( int i = 0; i != N; i++ )
-    {
-        nodes[i] = hwid * nodes[i] +midp;
-        weights[i] = hwid * weights[i];
+        nodes[N - 1 - i] = -nodes[i];
+        weights[N - 1 - i] = -weights[i];
     }
 }
 
@@ -98,3 +81,48 @@ void LegendreP_order_N(int N, double x, double &y, double &dy)
     y = p; dy = dp;
 }
 
+void transform_GaussQuad_unit2x0inf(double x0, size_t N, double *nodes,
+                                    double *weights)
+{
+    for (auto i = 0; i != N; i++)
+    {
+        weights[i] = weights[i] / ((1.0 - nodes[i]) * (1.0 - nodes[i]));
+        nodes[i] = 0.5 * (nodes[i] + 1.0) / (1.0 - nodes[i]) + x0;
+    }
+}
+
+void transform_GaussQuad_unit2x0x1(double x0, double x1, size_t N, double *nodes, double *weights)
+{
+    double midp = (x0 + x1) * 0.5;
+    double hwid = (x1 - x0) * 0.5;
+
+    for (auto i = 0; i != N; i++)
+    {
+        nodes[i] = hwid * nodes[i] + midp;
+        weights[i] = hwid * weights[i];
+    }
+}
+
+void transform_GaussQuad_unit2inf(size_t N, double *nodes, double *weights)
+{
+    double xsq;
+
+    for (auto i = 0; i != N; i++)
+    {
+        xsq = nodes[i] * nodes[i];
+        weights[i] =
+            weights[i] * 5.0 * (1.0 + xsq) / ((1.0 - xsq) * (1.0 - xsq));
+        nodes[i] = 5.0 * nodes[i] / (1.0 - xsq);
+    }
+}
+
+void transform_GaussQuad_unit2minfx0(double x0, size_t N, double *nodes, double *weights)
+{
+    int i;
+
+    for (i = 0; i != N; ++i)
+    {
+        weights[i] = weights[i] / ((1.0 + nodes[i]) * (1.0 + nodes[i]));
+        nodes[i] = 0.5 * (nodes[i] - 1.0) / (1.0 + nodes[i]) + x0;
+    }
+}
--- src/mathtools.h
+++ src/mathtools.h
@@ -1,12 +1,25 @@
 #pragma once
 #include <cstddef>
 
+//! Get the Gauss-Chebyshev quadrature based on Chebyshev points of the first type (kind)
 void GaussChebyshevI_unit(size_t N, double *nodes, double *weights);
+//! Get the Gauss-Chebyshev quadrature based on Chebyshev points of the second type (kind)
 void GaussChebyshevII_unit(size_t N, double *nodes, double *weights);
-void GaussLegendre_unit(size_t N, double *nodes, double *weights);
 
-void transform_GaussQuad_unit2x0inf(double x0, size_t N, double *nodes, double *weights);
-void transform_GaussQuad_unit2x0x1(double x0, double x1, size_t N, double *nodes, double *weights);
+//! Compute N-point Gauss-Legendre grids in [-1, 1];
+void GaussLegendre_unit(size_t N, double *nodes, double *weights);
 
 //! Compute Legendre polynomial of order N at x in [-1, 1];
 void LegendreP_order_N(int N, double x, double &y, double &dy);
+
+//! Transform Gauss grids from [-1, 1] to [x0, infty]
+void transform_GaussQuad_unit2x0inf(double x0, size_t N, double *nodes, double *weights);
+
+//! Transform Gauss grids from [-1, 1] to [x0, x1]
+void transform_GaussQuad_unit2x0x1(double x0, double x1, size_t N, double *nodes, double *weights);
+
+//! Transform Gauss grids from [-1, 1] to [-infty, infty]
+void transform_GaussQuad_unit2inf(size_t N, double *nodes, double *weights);
+
+//! Transform Gauss grids from [-1, 1] to [-infty, x0]
+void transform_GaussQuad_unit2minfx0(double x0, size_t N, double *nodes, double *weights);
