49247f26a940da611502e5e1ad0ad0962fa2f650
Huanjing Gong <gonghuanjing@iphy.ac.cn>
2025-01-10 19:54:37 +0800

    rebase: update

--- driver/driver_utils.cpp
+++ driver/driver_utils.cpp
@@ -170,14 +170,15 @@ std::vector<double> interpolate_dielec_func(int option, const std::vector<double
                 // read_eigenvector("./pyatb_librpa_df/", pyatb_meanfield);
 
                 read_velocity(file_abacus, meanfield);
-                std::vector<Vector3_Order<double>> kfrac_band = read_band_kpath_info(
-                    n_basis, n_states, n_spin, "./pyatb_librpa_df/k_path_info");
+                std::vector<Vector3_Order<double>> kfrac_band =
+                    read_band_kpath_info(driver_params.input_dir + "pyatb_librpa_df/k_path_info",
+                                         n_basis, n_states, n_spin);
                 df_headwing.set(meanfield, kfrac_band, frequencies_target, n_basis, n_states,
                                 n_spin);
             }
             else if (infile_aims.is_open())
             {
-                read_velocity_aims(meanfield, "./");
+                read_velocity_aims(meanfield, driver_params.input_dir);
                 n_basis = meanfield.get_n_aos();
                 n_states = meanfield.get_n_bands();
                 n_spin = meanfield.get_n_spins();
@@ -201,8 +202,8 @@ std::vector<double> interpolate_dielec_func(int option, const std::vector<double
         {
             int n_basis, n_states, n_spin;
 
-            std::string file_abacus = "./pyatb_librpa_df/velocity_matrix";
-            std::string file_aims = "./moment_KS_spin_01_kpt_000001.dat";
+            std::string file_abacus = driver_params.input_dir + "pyatb_librpa_df/velocity_matrix";
+            std::string file_aims = driver_params.input_dir + "moment_KS_spin_01_kpt_000001.dat";
             ifstream infile_abacus;
             ifstream infile_aims;
             infile_abacus.open(file_abacus);
@@ -210,14 +211,15 @@ std::vector<double> interpolate_dielec_func(int option, const std::vector<double
             if (infile_abacus.is_open())
             {
                 read_velocity(file_abacus, meanfield);
-                std::vector<Vector3_Order<double>> kfrac_band = read_band_kpath_info(
-                    n_basis, n_states, n_spin, "./pyatb_librpa_df/k_path_info");
+                std::vector<Vector3_Order<double>> kfrac_band = kfrac_band =
+                    read_band_kpath_info(driver_params.input_dir + "pyatb_librpa_df/k_path_info",
+                                         n_basis, n_states, n_spin);
                 df_headwing.set(meanfield, kfrac_band, frequencies_target, n_basis, n_states,
                                 n_spin);
             }
             else if (infile_aims.is_open())
             {
-                read_velocity_aims(meanfield, "./");
+                read_velocity_aims(meanfield, driver_params.input_dir);
                 n_basis = meanfield.get_n_aos();
                 n_states = meanfield.get_n_bands();
                 n_spin = meanfield.get_n_spins();
