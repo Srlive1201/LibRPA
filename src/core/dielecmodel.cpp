bcc0a839c500fa0e2c093e13e4dc327a0db1f74a
Huanjing Gong <gonghuanjing@iphy.ac.cn>
2025-01-24 15:29:32 +0800

    fix: q=0 diagonalize cut Coulomb

--- src/core/dielecmodel.cpp
+++ src/core/dielecmodel.cpp
@@ -632,9 +632,10 @@ void diele_func::get_Xv(double vq_threshold, const librpa_int::atpair_k_cplx_mat
         const auto Mu = Mu_Nu.first;
         const auto Nu = Mu_Nu.second;
         // ofs_myid << "Mu " << Mu << " Nu " << Nu << endl;
-        if (Vq.count(Mu) == 0 || Vq.at(Mu).count(Nu) == 0 || Vq.at(Mu).at(Nu).count(q) == 0)
+        if (Vq_cut.count(Mu) == 0 || Vq_cut.at(Mu).count(Nu) == 0 ||
+            Vq_cut.at(Mu).at(Nu).count(q) == 0)
             continue;
-        auto Vq_cpl = *(Vq.at(Mu).at(Nu).at(q));
+        auto Vq_cpl = *(Vq_cut.at(Mu).at(Nu).at(q));
         const auto &Vq0 = std::make_shared<matrix>(Vq_cpl.real());
         // const auto &Vq0 = Vq.at(Mu).at(Nu).at(q);
         const auto n_mu = atomic_basis_abf_.get_atom_nb(Mu);
