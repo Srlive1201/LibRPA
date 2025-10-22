955872c87a8d5023f2003c92cfd13baf05682734
Minye Zhang <minyez.physchem@gmail.com>
2025-10-22 13:37:16 +0200

    test: complete test case for AP->BLACS redistribution

--- src/test/test_atomic_basis_blacs.cpp
+++ src/test/test_atomic_basis_blacs.cpp
@@ -2,14 +2,16 @@
 #include "../envs_mpi.h"
 #include "../utils_atomic_basis_blacs.h"
 
+#include "../matrix_m.h"
+#include "../matrix_m_parallel_utils.h"
+
 #include "../stl_io_helper.h"
 #include "testutils.h"
 
 #include <stdexcept>
 #include <cassert>
-#include <unordered_set>
 
-void test_ap_to_2d_global_indices_communicate()
+static void test_ap_to_2d_global_indices_communicate()
 {
     using namespace LIBRPA::envs;
 
@@ -166,7 +168,7 @@ void test_ap_to_2d_global_indices_communicate()
 }
 
 
-void test_ap_to_2d_local_indices_communicate()
+static void test_ap_to_2d_local_indices_communicate()
 {
     using namespace LIBRPA::envs;
 
@@ -264,6 +266,272 @@ void test_ap_to_2d_local_indices_communicate()
     blacs_ctxt_global_h.exit();
 }
 
+// Example of performing transformation from atom-pair distribution to BLACS disctribution
+static void test_ap_to_2d_matrix_m_communicate()
+{
+    using namespace LIBRPA::envs;
+
+    const size_t m = 4;
+    const size_t n = m;
+    blacs_ctxt_global_h.set_square_grid(true, LIBRPA::CTXT_LAYOUT::R);
+    // Process grid:
+    //    0  1
+    //    2  3
+    LIBRPA::Array_Desc ad(blacs_ctxt_global_h);
+    ad.init_1b1p(m, n, 0, 0);
+    assert(ad.initialized());
+    assert(ad.mb() == 2);
+    assert(ad.nb() == 2);
+
+    LIBRPA::AtomicBasis ab;
+
+    // 3 atoms, atom 0 and 2 with 1 and atom 1 with 2
+    // Process indices
+    // | 0   0 | 0   1 |
+    // | 0   3 | 3   1 |
+    // |-------|-------|
+    // | 0   3 | 3   1 |
+    // | 2   2 | 2   3 |
+    ab.set(std::vector<size_t>{1, 2, 1});
+    assert(ab.nb_total == m);
+    {
+        // initialize data
+        map<atpair_t, matrix_m<double>> data;
+        // Global matrix
+        // | -1  |  0   2  |  1 |
+        // |-----|---------|----|
+        // |  0  |  1   3  |  0 |
+        // |  2  |  3  -2  | -1 |
+        // |-----|---------|----|
+        // |  1  |  0  -1  |  1 |
+        if (myid_global == 0)
+        {
+             data[{0, 0}] = matrix_m<double>({{-1.0}}, MAJOR::COL);
+             assert(data.at({0, 0}).nr() == 1 && data.at({0, 0}).nc() == 1);
+             data[{1, 0}] = matrix_m<double>({{0.0}, {2.0}}, MAJOR::COL);
+             assert(data.at({1, 0}).nr() == 2 && data.at({1, 0}).nc() == 1);
+             data[{0, 1}] = matrix_m<double>({{0.0, 2.0}}, MAJOR::COL);
+             assert(data.at({0, 1}).nr() == 1 && data.at({0, 1}).nc() == 2);
+        }
+        else if (myid_global == 1)
+        {
+             data[{0, 2}] = matrix_m<double>({{1.0}}, MAJOR::COL);
+             assert(data.at({0, 2}).nr() == 1 && data.at({0, 2}).nc() == 1);
+             data[{1, 2}] = matrix_m<double>({{0.0}, {-1.0}}, MAJOR::COL);
+             assert(data.at({1, 2}).nr() == 2 && data.at({1, 2}).nc() == 1);
+        }
+        else if (myid_global == 2)
+        {
+             data[{2, 0}] = matrix_m<double>({{1.0}}, MAJOR::COL);
+             assert(data.at({2, 0}).nr() == 1 && data.at({2, 0}).nc() == 1);
+             data[{2, 1}] = matrix_m<double>({{0.0, -1.0}}, MAJOR::COL);
+             assert(data.at({2, 1}).nr() == 1 && data.at({2, 1}).nc() == 2);
+        }
+        else if (myid_global == 3)
+        {
+             data[{1, 1}] = matrix_m<double>({{1.0, 3.0}, {3.0, -2.0}}, MAJOR::COL);
+             assert(data.at({1, 1}).nr() == 2 && data.at({1, 1}).nc() == 2);
+             data[{2, 2}] = matrix_m<double>({{1.0}}, MAJOR::COL);
+             assert(data.at({2, 2}).nr() == 1 && data.at({2, 2}).nc() == 1);
+        }
+        // Reference BLACS local matrix
+        // | -1   0  |  2   1 |
+        // |  0   1  |  3   0 |
+        // |---------|--------|
+        // |  2   3  | -2  -1 |
+        // |  1   0  | -1   1 |
+        auto m_loc_ref = init_local_mat<double>(ad, MAJOR::COL);
+        m_loc_ref = 0.0;
+        assert(m_loc_ref.nr() == 2 && m_loc_ref.nc() == 2);
+        if (myid_global == 0)
+        {
+            m_loc_ref(0, 0) = -1.0;
+            m_loc_ref(1, 1) = 1.0;
+        }
+        else if (myid_global == 1)
+        {
+            m_loc_ref(0, 0) = 2.0;
+            m_loc_ref(1, 0) = 3.0;
+            m_loc_ref(0, 1) = 1.0;
+        }
+        else if (myid_global == 2)
+        {
+            m_loc_ref(0, 0) = 2.0;
+            m_loc_ref(1, 0) = 1.0;
+            m_loc_ref(0, 1) = 3.0;
+        }
+        else if (myid_global == 3)
+        {
+            m_loc_ref(0, 0) = -2.0;
+            m_loc_ref(1, 0) = -1.0;
+            m_loc_ref(0, 1) = -1.0;
+            m_loc_ref(1, 1) = 1.0;
+        }
+
+        // Initialize BLACS local buffer
+        auto m_loc = init_local_mat<double>(ad, MAJOR::COL);
+        assert(m_loc.nr() == 2 && m_loc.nc() == 2);
+        // Fill in data that is already available before communication
+        int I, J, i, j;
+        for (int ir = 0; ir < m_loc.nr(); ir++)
+        {
+            ab.get_local_index(ad.indx_l2g_r(ir), I, i);
+            for (int ic = 0; ic < m_loc.nc(); ic++)
+            {
+                ab.get_local_index(ad.indx_l2g_c(ic), J, j);
+                const atpair_t atpair{static_cast<atom_t>(I), static_cast<atom_t>(J)};
+                if (data.count(atpair))
+                {
+                    m_loc(ir, ic) = data.at(atpair)(i, j);
+                }
+            }
+        }
+
+        // compute indices
+        const auto proc2idlist = LIBRPA::utils::get_communicate_local_ids_list_ap_to_blacs(
+            myid_global,
+            {
+             {0, {{0, 0}, {1, 0}, {0, 1}}},
+             {1, {{0, 2}, {1, 2}}},
+             {2, {{2, 0}, {2, 1}}},
+             {3, {{1, 1}, {2, 2}}}
+            },
+            ab, ab,
+            ad, true, false);
+
+        const auto &pid_ids_send = proc2idlist.first;
+        const auto &pid_ids_recv = proc2idlist.second;
+
+        // prepare recv buffer
+        // computer recv buffer size and displacements
+        map<int, pair<int, MPI_Count>> pid_recv_disp_count;
+        std::vector<double> recvbuff(0);
+        MPI_Count recvcount = 0;
+        for (int pid = 0; pid < size_global; pid++)
+        {
+            if (pid_ids_recv.count(pid))
+            {
+                pid_recv_disp_count[pid] = {static_cast<int>(recvcount), pid_ids_recv.at(pid).size()};
+                recvcount += pid_ids_recv.at(pid).size();
+            }
+        }
+        if (recvcount > 0) recvbuff.resize(recvcount);
+
+        // prepare send buffer
+        // first run, computer send buffer size and displacements, allocate the whole buffer
+        map<int, pair<int, MPI_Count>> pid_send_disp_count;
+        std::vector<double> sendbuff(0);
+        MPI_Count sendcount = 0;
+        for (int pid = 0; pid < size_global; pid++)
+        {
+            if (pid_ids_send.count(pid))
+            {
+                const auto &ids_send = pid_ids_send.at(pid);
+                MPI_Count sendcount_pid = 0;
+                for (const auto &pair_ids: ids_send)
+                {
+                    sendcount_pid += pair_ids.second.size();
+                }
+                pid_send_disp_count[pid] = {static_cast<int>(sendcount), sendcount_pid};
+                sendcount += sendcount_pid;
+            }
+        }
+        if (sendcount > 0) sendbuff.resize(sendcount);
+        // second run, fill in the data to be sent
+        for (int pid = 0; pid < size_global; pid++)
+        {
+            if (pid_ids_send.count(pid))
+            {
+                const auto &ids_send = pid_ids_send.at(pid);
+                MPI_Count disp = pid_send_disp_count.at(pid).first;
+                for (const auto &pair_ids: ids_send)
+                {
+                    const auto &atpair = pair_ids.first;
+                    const auto &atmat = data.at(atpair);
+                    const auto &ids = pair_ids.second;
+                    for (auto i = 0; i < ids.size(); i++)
+                    {
+                        const auto &id = ids[i];
+                        sendbuff[disp+i] = atmat.ptr()[id];
+                    }
+                    disp += ids.size();
+                }
+            }
+        }
+
+        // Begin communication
+        std::vector<MPI_Request> reqs;
+        MPI_Request req;
+        // First non-blocking receive
+        for (const auto &pid_disp_count: pid_recv_disp_count)
+        {
+            const auto &pid = pid_disp_count.first;
+            const auto &disp_count = pid_disp_count.second;
+            const auto &disp = disp_count.first;
+            const auto &count = disp_count.second;
+            // ofs << "MPI_Irecv dist " << dist << " count " << count << " from pid " << pid << endl;
+            MPI_Irecv(recvbuff.data() + disp, count, MPI_DOUBLE, pid, 0, MPI_COMM_WORLD, &req);
+            reqs.push_back(req);
+        }
+        // Then non-blocking send
+        for (const auto &pid_disp_count: pid_send_disp_count)
+        {
+            const auto &pid = pid_disp_count.first;
+            const auto &disp_count = pid_disp_count.second;
+            const auto &disp = disp_count.first;
+            const auto &count = disp_count.second;
+            // ofs << "MPI_Isend count " << count << " to pid " << pid << endl;
+            MPI_Isend(sendbuff.data() + disp, count, MPI_DOUBLE, pid, 0, MPI_COMM_WORLD, &req);
+            reqs.push_back(req);
+        }
+
+        // Wait all non-blocking communication to finish
+        if (!reqs.empty()) MPI_Waitall((int)reqs.size(), reqs.data(), MPI_STATUSES_IGNORE);
+
+        // // Intermediate check
+        // for (int i = 0; i < 4; i++)
+        // {
+        //     blacs_ctxt_global_h.barrier();
+        //     if (myid_global == i)
+        //     {
+        //         std::cout << "myid " << i << std::endl;
+        //         std::cout << "Send buffer:" << sendbuff << std::endl;
+        //         std::cout << "Recv buffer:" << recvbuff << std::endl;
+        //     }
+        // }
+
+        // Map the received data to the correct location in the BLACS sub-block
+        for (const auto &pid_disp_count: pid_recv_disp_count)
+        {
+            const auto &pid = pid_disp_count.first;
+            const auto &ids = pid_ids_recv.at(pid);
+            const auto &disp_count = pid_disp_count.second;
+            const auto &disp = disp_count.first;
+            const auto &count = disp_count.second;
+            for (auto i = 0; i < count; i++)
+            {
+                m_loc.ptr()[ids[i]] = recvbuff[disp+i];
+            }
+        }
+
+        // Final check
+        for (int i = 0; i < 4; i++)
+        {
+            blacs_ctxt_global_h.barrier();
+            if (myid_global == i)
+            {
+                std::cout << "myid " << i << std::endl;
+                assert(fequal_array(m_loc_ref.size(), m_loc_ref.ptr(), m_loc.ptr(), true));
+                // fequal_array(m_loc_ref.size(), m_loc_ref.ptr(), m_loc.ptr(), true);
+                // std::cout << "Local matrix:" << std::endl << m_loc << std::endl;
+            }
+        }
+    }
+
+    blacs_ctxt_global_h.exit();
+}
+
+
 int main (int argc, char *argv[])
 {
     using namespace LIBRPA::envs;
@@ -279,6 +547,7 @@ int main (int argc, char *argv[])
     // test functions begin
     test_ap_to_2d_global_indices_communicate();
     test_ap_to_2d_local_indices_communicate();
+    test_ap_to_2d_matrix_m_communicate();
     // test functions end
 
     finalize_blacs();
