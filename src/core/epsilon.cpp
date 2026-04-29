96bffa9ca11d042e4c2af8af336d28c2061ec4db
userchba <88939318+userchba@users.noreply.github.com>
2026-04-29 17:43:05 +0800

    fix a bug with rpa correlation
    fix runtime error for rpa correlation gamma only in multi process situation

--- src/core/epsilon.cpp
+++ src/core/epsilon.cpp
@@ -75,17 +75,6 @@ CorrEnergy compute_RPA_correlation_blacs_2d_gamma_only(Chi0 &chi0, atpair_k_cplx
     auto coul_block = init_local_mat<double>(desc_nabf_nabf, MAJOR::COL);
     auto coul_chi0_block = init_local_mat<double>(desc_nabf_nabf, MAJOR::COL);
 
-    vector<Vector3_Order<double>> qpts;
-    // for (const auto &qMuNuchi : chi0.get_chi0_q().at(chi0.tfg.get_freq_nodes()[0]))
-    //     qpts.push_back(qMuNuchi.first);
-    for (const auto &q : chi0.pbc.klist)
-    {
-        qpts.push_back(q);
-#ifdef OPEN_TEST_FOR_LU_DECOMPOSITION
-        printf("processId:%d, q: (%f, %f, %f)\n", blacs_h.myid, q.x, q.y, q.z);
-#endif
-    }
-
     const auto &klist = chi0.pbc.klist;
     const auto &map_ibzk_weight = chi0.pbc.map_ibzk_weight;
     complex<double> tot_RPA_energy(0.0, 0.0);
@@ -93,7 +82,7 @@ CorrEnergy compute_RPA_correlation_blacs_2d_gamma_only(Chi0 &chi0, atpair_k_cplx
     if(comm_h.is_root())
         lib_printf("Finish init RPA blacs 2d\n");
 #ifdef LIBRPA_USE_LIBRI
-    for (const auto &q : qpts)
+    for (const auto &q: klist)
     {
         coul_block.zero_out();
 
@@ -163,7 +152,6 @@ CorrEnergy compute_RPA_correlation_blacs_2d_gamma_only(Chi0 &chi0, atpair_k_cplx
                 double chi_begin_arr = omp_get_wtime();
                 std::map<int, std::map<std::pair<int, std::array<double, 3>>, Tensor<double>>>
                     chi0_libri;
-                // const auto &chi0_wq = chi0.get_chi0_q().at(freq).at(q);
                 atom_mapping<ComplexMatrix>::pair_t_old chi0_wq;
                 if (!chi0.get_chi0_q().empty()) chi0_wq = chi0.get_chi0_q().at(freq).at(q);
 
